87ab0e482fb952c15519b69b7764c4fc29a2236d
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2026-01-08 12:40:51 -0500

    dev: add flake environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Stanford CS143 Compiler COOL C++";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-25.11;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    # with flake-utils.lib; eachSystem allSystems (system:
+    with flake-utils.lib; eachSystem ["x86_64-linux" "aarch64-darwin"] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # riscvCross = import nixpkgs {
+        #   localSystem = "${system}";
+        #   crossSystem = {
+        #     config = "riscv64-unknown-linux-gnu";
+        #   };
+        # };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            flex
+            bison
+            clang-tools # for clangd lsp
+
+            cmake
+            gcc
+          ];
+        };
+      });
+}
