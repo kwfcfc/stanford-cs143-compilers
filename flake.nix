1af305e7c3860be4391e1695b5802892fda9f2f0
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2026-01-09 14:33:54 -0500

    dev: merge nix flake and gitignore from PA3

--- flake.nix
+++ flake.nix
@@ -17,16 +17,45 @@
         #   };
         # };
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            git
-            flex_2_5_35
-            bison
-            clang-tools # for clangd lsp
+        devShells.default = let
+          # scripts for referene compiler components
+          coolc = pkgs.stdenv.mkDerivation {
+            name = "coolc";
+            src = ./bin;
 
-            cmake
-            gcc
-          ];
-        };
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r . $out/bin/
+              chmod +x $out/bin/*
+            '';
+          };
+        in
+          pkgs.mkShell {
+            packages = [
+              coolc
+            ];
+            buildInputs = with pkgs; [
+              git
+              flex_2_5_35
+              bison
+              clang-tools # for clangd lsp
+
+              # perl and sed for grading script
+              perl
+              gnused
+
+              #
+
+              cmake
+              gcc
+            ];
+          };
+
+        # shellHook 会在进入 nix develop 时运行
+        shellHook = ''
+          # echo "fixing script executable path..."
+          # patchShebangs scripts
+          patchShebangs ./assignments/
+        '';
       });
 }
